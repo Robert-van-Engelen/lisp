c9df72ad95a52da6ce150751059de8ecf2add849
Robert van Engelen <88845949+Robert-van-Engelen@users.noreply.github.com>
2022-08-16 10:22:28 -0400

    updates

--- examples/apply.lisp
+++ examples/apply.lisp
@@ -4,30 +4,6 @@
 ; The '_ symbol is used as a temporary let* variable in the constructed macro code to avoid name clashes
 ; For example: (apply + 0 1 (list 2 3)) => (let* (_ (list 0 1 3 4)) (+ . _))
 
-;(defmacro apply (f . args)
-;    (letrec*
-;        (c (lambda (t)
-;            (if (cdr t)
-;                (cons (car t) (c (cdr t)))
-;                (eval (car t)))))
-;        (list 'let* (list '_ (c (cons 'list args))) (cons f '_))))
-
-;(defmacro apply fargs
-;    (letrec*
-;        (c (lambda (t s)
-;            (if (cdr t)
-;                (c (cdr t) (cons (car t) s))
-;                (list (cons '_ t) s))))
-;        (cons 'let* (c fargs '_))))
-
-;(defmacro apply fargs
-;    (letrec*
-;        (c (lambda (t s)
-;            (if (cdr t)
-;                (c (cdr t) (cons (car t) s))
-;                (list 'let* (cons '_ t) s))))
-;        (c fargs '_)))
-
 (defmacro apply fargs
     (letrec*
         (del (lambda (t)
