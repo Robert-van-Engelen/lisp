bd61fe4dfc9f16e162ce9da24a1018bc7d3347f3
Robert van Engelen <88845949+Robert-van-Engelen@users.noreply.github.com>
2022-08-16 10:24:00 -0400

    updates

--- examples/apply.lisp
+++ examples/apply.lisp
@@ -2,7 +2,7 @@
 ; Requires init.lisp
 ; This is a redundant function because in most cases the dot operator can be used to accomplish the same
 ; The '_ symbol is used as a temporary let* variable in the constructed macro code to avoid name clashes
-; For example: (apply + 0 1 (list 2 3)) => (let* (_ (list 0 1 3 4)) (+ . _))
+; For example: (apply + 0 1 (list 2 3)) => (let* (_ (list 3 4)) (+ 0 1 . _))
 
 (defmacro apply fargs
     (letrec*
